16b0dcfded3c404d614ed954bfdf3cae30b3704f
bil <bubenchikov@benzo.ru>
2025-08-28 17:33:32 +0500

    Добавил метод отображение у модели в админке

--- app_run/models.py
+++ app_run/models.py
@@ -7,3 +7,6 @@ class Run(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.athlete.username + " - " + self.comment
