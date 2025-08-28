e6071c8e740bee4b1a7f91d841a6f1dcc5c31ca7
bil <bubenchikov@benzo.ru>
2025-08-28 17:04:17 +0500

    Добавил новую модель по атлетам

--- app_run/models.py
+++ app_run/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Run(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField()
+    athlete = models.ForeignKey(User, on_delete=models.CASCADE)
