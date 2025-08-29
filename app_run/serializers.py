13dec2d65dfc2098d63b5f12a14967796a90b572
bil <bubenchikov@benzo.ru>
2025-08-29 17:51:03 +0500

    Добавил обработку получения данных по всем users. Нужно доделать фильтр по условию задачи

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from app_run.models import Run
@@ -7,3 +8,17 @@ class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
         fields = "__all__"
+
+
+class CouchAthleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "is_staff",
+            "is_superuser",
+        )
--- app_run/views.py
+++ app_run/views.py
@@ -1,10 +1,11 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from app_run.models import Run
-from app_run.serializers import RunSerializer
+from app_run.serializers import RunSerializer, CouchAthleteSerializer
 
 
 # Create your views here.
@@ -21,3 +22,15 @@ def company_details(request):
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+
+class CouchAthleteViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = CouchAthleteSerializer
+
+    def get_queryset(self):
+        qs = self.request
+        type = self.request.query_params.get("type", None)
+        print(type)
+
+        return User.objects.filter(is_superuser=False)
