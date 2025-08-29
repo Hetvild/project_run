d282d53d078dd67d0a6370544833ad7d63b19b07
bil <bubenchikov@benzo.ru>
2025-08-29 18:36:25 +0500

    Исправил ошибку: добавил в ответ поле date_joined

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -18,12 +18,10 @@ class CouchAthleteSerializer(serializers.ModelSerializer):
         model = User
         fields = (
             "id",
+            "date_joined",
             "username",
-            "first_name",
             "last_name",
-            "email",
-            "is_staff",
-            "is_superuser",
+            "first_name",
             "type",
         )
 
