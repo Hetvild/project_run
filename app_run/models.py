1863b7586294010d90a4170c915dbb707dca1c27
bil <bubenchikov@benzo.ru>
2025-09-18 16:26:03 +0500

    Задача 16. Создал миграцию 0022 с правильным полем для date_time

--- app_run/models.py
+++ app_run/models.py
@@ -48,10 +48,8 @@ class Position(models.Model):
     run = models.ForeignKey(Run, on_delete=models.CASCADE, related_name="positions")
     latitude = models.FloatField()
     longitude = models.FloatField()
-    date_time = models.CharField(default="")
-    # date_time = models.DateTimeField(
-    #     null=True, blank=True, help_text="Формат: YYYY-MM-DDTHH:MM:SSZ"
-    # )
+    # date_time = models.CharField(default="")
+    date_time = models.DateTimeField(null=True, blank=True)
 
 
 class CollectibleItem(models.Model):
